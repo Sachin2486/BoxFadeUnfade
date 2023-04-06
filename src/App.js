038540a7e1c20494de6aa63546499b51864c6d26
SachinTiwari <65590299+Sachin2486@users.noreply.github.com>
2023-04-06 10:51:33 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,96 @@
+// import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import './App.css';
+
+let order = 0;
+let isAllClicked = false;
+function App() {
+
+
+  const { boxState, setBoxState } = useState(getBoxes("initial"));
+
+
+  useEffect(() => {
+
+
+    if (boxState.some((box) => !box.isClicked)) {
+
+      isAllClicked = false;
+    }
+    else {
+      isAllClicked = true;
+    }
+
+    if (isAllClicked) {
+
+      boxState.forEach((item, index) => {
+
+        return setTimeout(() => {
+
+          let tempBox = [...boxState]
+          tempBox[index].isClicked = false;
+          setBoxState(tempBox);
+
+        }, 1000 * (index + 1))
+      })
+    }
+
+  }, [boxState]);
+
+  function getBoxes(type) {
+    let boxesData = [];
+
+    const boxes = [0, 1, 2].map((i) => {
+
+      return [0, 1, 2].map((j) => {
+
+        if (!(i === 1 && j > 0)) {
+
+
+          if (type === 'initial') {
+            return boxesData.push({ i, j, isClicked: false, order: null })
+          }
+
+          return <div style={{
+            backgroundColor: boxState?.find((item) => item.i === i && item.j === j)?.isClicked && "green",
+          }} onClick={() => changeColor(i, j)} className="box"></div>
+
+        }
+
+        return <div></div>;
+
+      });
+    });
+
+
+    if (type === 'initial') {
+      return boxesData;
+    }
+
+    return boxes;
+
+  }
+
+
+
+  const changeColor = (i, j) => {
+
+
+    let temp = [...boxState]
+    const selectedBox = temp.find((item) => item.i === i && item, j === j);
+    selectedBox.isClicked = true;
+    selectedBox.order = ++order;
+    temp.sort((a, b) => a.order > b.order ? 1 : -1); //to remove the green patch back after the user clicks back
+    setBoxState(temp);
+
+  }
+
+  return (
+    <div className="App">
+
+      <div className="box-container">{getBoxes()}</div>
+    </div>
+  );
+}
+
+export default App;
